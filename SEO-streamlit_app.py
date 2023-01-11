bbe9189dfca0d679ea4ffd5d408eeec2f31ccd0e
AIQueen <105072501+AIqueen7@users.noreply.github.com>
2023-01-11 15:31:45 -0700

    Update SEO-streamlit_app.py

--- SEO-streamlit_app.py
+++ SEO-streamlit_app.py
@@ -50,7 +50,7 @@ st.set_page_config(
 
 # The Streamlit app main section
 st.title("AIQueen SEO App")
-st.write("Make your ideas real.")
+
 
 input_google_autocomplete_keyword: str = st.text_input(
     "What is your seed keyword?")
